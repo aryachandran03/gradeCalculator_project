e332631f5c58952c493402a2fc31c4c9a60309c0
Arya <aryachandran03@gmail.com>
2025-12-16 00:55:17 +0400

    Add full project

--- JS/index.js
+++ JS/index.js
@@ -1,40 +1,39 @@
-// Function for grade calculation
-function calculateGrade() {
-    //alert('Hi');
-    // Taking marks as inputs
-    let sub1 = parseFloat(prompt("Enter marks for English (0-100):").trim());
-    let sub2 = parseFloat(prompt("Enter marks for Hindi (0-100):").trim());
-    let sub3 = parseFloat(prompt("Enter marks for Mathematics (0-100):").trim());
-    let sub4 = parseFloat(prompt("Enter marks for Science (0-100):").trim());
-    let sub5 = parseFloat(prompt("Enter marks for Social Science (0-100):").trim());
-
-    // Mark value validation
-
-    if (isNaN(sub1) || isNaN(sub2) || isNaN(sub3) || isNaN(sub4) || isNaN(sub5)) {
-        alert("Enter valid marks");
-        return;
-    }
-
-    // Calculating total and average
-    const subTotal = sub1 + sub2 + sub3 + sub4 + sub5;
-    const numberOfSubjects = 5;
-    const Average = subTotal / numberOfSubjects;
-
-    // Finding the grade
-    let Grade = "";
-    if (Average >= 90)
-        Grade = "A+";
-    else if (Average >= 80)
-        Grade = "A";
-    else if (Average >= 70)
-        Grade = "B";
-    else if (Average >= 60)
-        Grade = "C";
-    else if (Average >= 50)
-        Grade = "D";
-    else
-        Grade = "F";
-
-    //Show the grade
-    alert("Congratulations! Your Grade is: " + Grade + " and your average mark is " + Average);
+// function to add the numbers
+function add() {
+    let a = Number(document.getElementById("number_1").value);
+    let b = Number(document.getElementById("number_2").value);
+    document.getElementById("result").innerHTML = "Result: " + (a + b);
 }
+// function to subtract the numbers
+function subtract() {
+        let a = Number(document.getElementById("number_1").value);
+        let b = Number(document.getElementById("number_2").value);
+        document.getElementById("result").innerHTML = "Result: " + (a - b);
+    }
+// function to multiply the numbers
+    function multiply() {
+        let a = Number(document.getElementById("number_1").value);
+        let b = Number(document.getElementById("number_2").value);
+        document.getElementById("result").innerHTML = "Result: " + (a * b);
+    }
+// function to divide the numbers
+    function divide() {
+        let a = Number(document.getElementById("number_1").value);
+        let b = Number(document.getElementById("number_2").value);
+
+        if (b === 0) {
+            document.getElementById("result").innerHTML = "Cannot divide by zero";
+        } else {
+            document.getElementById("result").innerHTML = "Result: " + (a / b);
+        }
+    }
+// function to find square the numbers
+    function square() {
+        let a = Number(document.getElementById("number_1").value);
+        document.getElementById("result").innerHTML = "Square of the first number: " + (a * a);
+    }
+// function to find cube of the numbers
+    function cube() {
+        let a = Number(document.getElementById("number_1").value);
+        document.getElementById("result").innerHTML = "Cube of the first numbers: " + (a * a * a);
+    }
